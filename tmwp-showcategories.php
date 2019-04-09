29f00c2aa065975184fca1c0aaf18cbecd16e2b1
Marcel Reschke <hello@marcelreschke.com>
2019-04-09 17:41:08 +0200

    Add tmwp-showcategories.php

---
+++ tmwp-showcategories.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: TailormadeWP Show Categories
+ * Plugin URI:  https://www.tailormadewp.com/plugins/tmwp-showcategories/
+ * Description: Sophisticated category-listing
+ * Version:     0.0.0
+ * Author:      TailormadeWP
+ * Author URI:  https://www.tailormadewp.com/
+ * License:     GPLv2
+ * Text Domain: tmwp-showcategories
+ * Domain Path: /languages
+ *
+ * @link    https://www.tailormadewp.com/plugins/tmwp-showcategories/
+ *
+ * @package TMWP_Show_Categories
+ * @version 0.0.0
+ */
+
+/**
+ * Copyright (c) 2019 TailormadeWP (email : hello@tailormadewp.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
